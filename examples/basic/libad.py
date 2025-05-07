4adf3493183755f6bfc49b4e4280a135a7a43e0e
stackotter <stackotter@stackotter.dev>
2025-05-08 02:55:44 +1000

    Expand libad.py to use new APIs, implement demo pastebin exploit

--- examples/basic/libad.py
+++ examples/basic/libad.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 from pwn import remote, context
+from typing import Union, Optional
 import sys
+import requests
+import re
 
 game_server = "10.40.0.1"
 flag_submission_port = 5000
+
+flag_regex = re.compile(r"flag\{[a-zA-Z0-9_\-\.]*\}")
 
 def submit_flag(flag: str) -> str:
     tmp = context.log_level
@@ -18,13 +23,181 @@ def submit_flag(flag: str) -> str:
 
     return response
 
-def usage():
+def extract_flags(content: str) -> list[str]:
+    return [match for match in flag_regex.findall(content)]
+
+def extract_flag(content: str) -> Optional[str]:
+    flags = flag_regex.findall(content)
+    if len(flags) == 0:
+        raise Exception(f"No flag found in '{content}'")
+    return flags[0]
+
+class Team:
+    id: int
+    name: str
+    ip: str
+    is_self: bool
+
+    def __init__(self, id: int, name: str, ip: str, is_self: bool):
+        self.id = id
+        self.name = name
+        self.ip = ip
+        self.is_self = is_self
+
+class TeamStore:
+    teams: list[Team]
+
+    def __init__(self, teams: list[Team]):
+        self.teams = teams
+
+    # Finds a team by id or name. Names are case insensitive.
+    def team(self, identifier: Union[int, str]) -> Optional[Team]:
+        for team in self.teams:
+            if type(identifier) == int and team.id == identifier:
+                return team
+            elif type(identifier) == str and team.name.lower() == identifier.lower():
+                return team
+        return None
+
+_teams: Optional[TeamStore] = None
+
+def fetch_teams() -> TeamStore:
+    global _teams
+    response = requests.get(f"http://{game_server}/api/teams")
+    data = response.json()
+    store = TeamStore([
+        Team(team["id"], team["name"], team["ip"], team["self"])
+        for team in data
+    ])
+    _teams = store
+    return store
+
+# Finds a team by id or name. Names are case insensitive.
+def team(identifier: Union[int, str]) -> Optional[Team]:
+    if _teams is None:
+        fetch_teams()
+    return _teams.team(identifier)
+
+class Service:
+    id: int
+    name: str
+    port: int
+
+    def __init__(self, id: int, name: str, port: int):
+        self.id = id
+        self.name = name
+        self.port = port
+
+class ServiceStore:
+    services: list[Service]
+
+    def __init__(self, services: list[Service]):
+        self.services = services
+
+    # Finds a service by id or name. Names are case insensitive.
+    def service(self, identifier: Union[int, str]) -> Optional[Service]:
+        for service in self.services:
+            if type(identifier) == int and service.id == identifier:
+                return service
+            elif type(identifier) == str and service.name.lower() == identifier.lower():
+                return service
+        return None
+
+_services: Optional[ServiceStore] = None
+
+def fetch_services() -> ServiceStore:
+    global _services
+    response = requests.get(f"http://{game_server}/api/vulnbox/services")
+    data = response.json()
+    store = ServiceStore([
+        Service(service["id"], service["name"], service["port"])
+        for service in data
+    ])
+    _services = store
+    return store
+
+# Finds a service by id or name. Names are case insensitive.
+def service(identifier: Union[int, str]) -> Optional[Team]:
+    if _services is None:
+        fetch_services()
+    return _services.service(identifier)
+
+class FlagId:
+    tick: int
+    team: int
+    service: int
+    value: str
+
+    def __init__(self, tick: int, team: int, service: int, value: str):
+        self.tick = tick
+        self.team = team
+        self.service = service
+        self.value = value
+
+    def __repr__(self) -> str:
+        return self.value
+
+class FlagIdStore:
+    _flag_ids: list[FlagId]
+
+    def __init__(self, flag_ids: list[FlagId]):
+        self._flag_ids = flag_ids
+
+    # Find all flag ids matching the given criteria. `team` and `service` can
+    # be either ids or names. Names are case insensitive.
+    def flag_ids(self, tick: int = None, team: Union[int, str] = None, service: Union[int, str] = None, skip_self: bool = False) -> list[FlagId]:
+        if _teams is None and (type(team) == str or skip_self):
+            fetch_teams()
+        if type(team) == str:
+            team = _teams.team(team).id
+
+        if type(service) == str:
+            if _services is None:
+                fetch_services()
+            service = _services.service(service).id
+
+        filtered = []
+        for flag_id in self._flag_ids:
+            if tick is not None and tick != flag_id.tick:
+                continue
+            if team is not None and team != flag_id.team:
+                continue
+            if service is not None and service != flag_id.service:
+                continue
+            if skip_self and _teams.team(flag_id.team).is_self:
+                continue
+            filtered.append(flag_id)
+        return filtered
+
+def fetch_flag_ids() -> FlagIdStore:
+    response = requests.get(f"http://{game_server}/api/flagIds")
+    data = response.json()
+
+    flag_ids = []
+    for flag_id in data:
+        flag_ids.append(FlagId(
+            flag_id["tick"],
+            flag_id["team"],
+            flag_id["service"],
+            flag_id["value"]
+        ))
+
+    return FlagIdStore(flag_ids)
+
+# Find all flag ids matching the given criteria. `team` and `service` can
+# be either ids or names. Names are case insensitive. Shorthand for fetching
+# then filtering. Doesn't cache.
+def flag_ids(tick: int = None, team: Union[int, str] = None, service: Union[int, str] = None, skip_self: bool = False) -> list[FlagId]:
+    flag_ids = fetch_flag_ids()
+    return flag_ids.flag_ids(tick=tick, team=team, service=service, skip_self=skip_self)
+
+def _usage():
     print("Usage: ./libad.py <command> <args ...>")
     print("Commands: [submit]")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        usage()
+        _usage()
         exit(1)
 
     cmd = sys.argv[1]
@@ -36,5 +209,5 @@ if __name__ == "__main__":
         for flag in flags:
             print(submit_flag(flag))
     else:
-        usage()
+        _usage()
         exit(1)
